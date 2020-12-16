22d34022eed2175c57d6a03e77c813d2a4061425
jennytang1224 <jennytang1224@gmail.com>
2020-12-16 15:59:39 -0800

    fixed slide bar

--- iceberg/ViewController.swift
+++ iceberg/ViewController.swift
@@ -7,15 +7,26 @@
 
 import UIKit
 
+
 class ViewController: UIViewController {
     
     @IBOutlet weak var billField: UITextField!
     
     @IBOutlet weak var tipLabel: UILabel!
     
+
     @IBOutlet weak var totalLabel: UILabel!
     
     @IBOutlet weak var tipControl: UISegmentedControl!
+    
+
+    @IBOutlet weak var amountPerPersonLabel: UILabel!
+
+    @IBOutlet weak var sliderValueLabel: UILabel!
+    
+    @IBOutlet weak var numberOfSplitsSlider: UISlider!
+    
+    var total : Double = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -30,15 +41,30 @@ class ViewController: UIViewController {
     @IBAction func calcTip(_ sender: Any) {
         // get the bill amount
         let bill = Double(billField.text!) ?? 0
-        
-        // calc the tip and total
+    
+        // calc the tip and total1
         let tipPercentages = [0.15,0.18,0.2]
         let tip = bill * tipPercentages[tipControl.selectedSegmentIndex]
-        let total = bill + tip
+        total = bill + tip
         
         // update the tip and total label
         tipLabel.text = String(format: "$%.2f", tip)
-        totalLabel.text = String(format: "$%.2f", total)
+        totalLabel.text = String(format: "%.2f", total)
     }
+
+    @IBAction func slideNumOfPeople(_ sender: Any) {
+        let numberOfPeople = Double(round(numberOfSplitsSlider.value))
+        sliderValueLabel.text = String(numberOfPeople)
+
+        total = Double(totalLabel.text!) ?? 0
+        //print(total1)
+        
+        // calculate amount per person
+        let amountPerPerson = (total/numberOfPeople)
+        // update amount per person
+        amountPerPersonLabel.text = String(format: "%.2f", amountPerPerson)
+    }
+    
+    
 }
 
